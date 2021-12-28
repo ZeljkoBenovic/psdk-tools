389551697238fe86a9569518e4bd23b6241b507b
ZeljkoBenovic <sakal87@gmail.com>
2021-12-28 16:56:41 +0000

    add environment feature

--- psdk-tools.py
+++ psdk-tools.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
 from psdk_commands import PsdkCommands
 
 if __name__ == "__main__":
+
   cmd = PsdkCommands()
   cmd.Run()
--- psdk_commands.py
+++ psdk_commands.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-import argparse
 import sys
+import argparse
 import os
-import subprocess
-import signal
-import shutil
-import platform
 import json
-# import local git package
-sys.path.append(os.path.dirname(__file__)+'/vendor/git')
-from git import Repo
 
-from helpers import UserInputBool
-
+from environments import Cloud, Localhost
 
 class PsdkCommands:
-    
+
+   
   def Run(self):
     
     self.__parser = argparse.ArgumentParser()
+
+    self.__parser.add_argument("-e","--environment",dest="environment",default="localhost",help="The environment you would like to run the nodes on. Default: localhost ( localhost, cloud, docker, terraform )")
+
+    self.__parser.add_argument("--hosts",dest="hosts",default=[],nargs="*",help="If environment is not local, with this flag you specify space delimited list of ip addresses of hosts.")
+    self.__parser.add_argument("-s",dest="ssh_key",default="~/.ssh/id_rsa.pub",help="If environment is not local, we'll need ssh key to authenticate to remote hosts. Default: ~/.ssh/id_rsa_pub")
+  
     self.__parser.add_argument("-c", "--command",dest="command",default="",help="The command you would like to run against all nodes. For multiple commands \" \" are required. Default: start new chain")
     self.__parser.add_argument("-b", "--branch",dest="branch",default="develop",help="PolygonSDK branch that will be cloned. Default: develop")
     self.__parser.add_argument("-d", "--dir",dest="clone_path",default="/tmp/polygon/polygon-sdk",help="Folder to clone repo into. Default: /tmp/polygon-sdk")
@@ -36,11 +35,29 @@ class PsdkCommands:
     
 
     self.__args = self.__parser.parse_args()
-    
+
+    self.__SetEnvironment()
     self.__RunCommand()
+
     exit()
 
+  def __SetEnvironment(self):
+
+    if self.__args.environment == "localhost":
+      # instantiate Localhost class and pass all cli args
+      self.__env = Localhost(self.__args)
+
+    elif self.__args.environment == "cloud":
+      # exit if no addresses are provided in cli
+      if self.__args.hosts == []:
+        sys.exit("Invalid host count. For Cloud environment, you need to define ip addresses of hosts")
+      # instantiate Cloud class and pass all args
+      self.__env = Cloud(self.__args)
+
+    else:
+      sys.exit("False environment set. Possible environments: localhost, cloud, docker and terraform")
     
+
   def __RunCommand(self):
     # run commands in multiple threads
 
@@ -48,10 +65,10 @@ class PsdkCommands:
       self.__StartNewChain()
 
     elif self.__args.command == 'stop':
-      self.__StopAllServers()
+      self.__env._StopAllServers()
 
     elif self.__args.command == 'start':
-      self.__StartServer()
+      self.__env._StartServer()
 
     else:
       self.__parser.print_help()
@@ -61,191 +78,16 @@ class PsdkCommands:
     # Store user settings to file
     self.__StoreSettings()
     # Fetch branch from repo
-    self.__FetchCode()
+    self.__env._FetchCode()
     # Build repo
-    self.__VerifyGo()
+    self.__env._VerifyGo()
     # Init psdk server
-    self.__InitPSDKServer()
+    self.__env._InitPSDKServer()
     # Generate genesis.json
-    self.__GenerateGenesisFile()
+    self.__env._GenerateGenesisFile()
     # Start polygon-sdk servers
-    self.__StartServer()
- 
-  # fetch git code from the specified branch
-  def __FetchCode(self) -> None:
-    if os.path.isdir(self.__args.clone_path) and UserInputBool("Existing repo found. Would you like to use the existing repo ? [y/N]"):
-      print("Using the existin repo.")
-      return
-    else:
-      print(f"Cloning branch {self.__args.branch}...")
-      # remove dir if exists
-      if os.path.isdir(f"{self.__args.clone_path}"):
-        shutil.rmtree(self.__args.clone_path)
-      # clone the repo
-      Repo.clone_from("https://github.com/0xPolygon/polygon-sdk.git",self.__args.clone_path,branch=self.__args.branch)
-      print(f"Branch {self.__args.branch} cloned.")
-  
-  # verify that go is available
-  def __VerifyGo(self) -> None:
+    self.__env._StartServer()
 
-    # add existing go binary to path
-    os.environ["PATH"] += os.pathsep + "/usr/local/go/bin"
-    # and check if go is installed
-    go_path = shutil.which("go")
-    # check the platform
-    os_system = platform.system()
-
-    # install go if not detected
-    if go_path == None and os_system == "Linux":
-      print("Go not installed. Installing...")
-      os.system("cd /tmp && curl -OL https://golang.org/dl/go1.17.5.linux-amd64.tar.gz")
-      os.system("sudo tar -C /usr/local -xf /tmp/go1.17.5.linux-amd64.tar.gz")
-      os.system("sudo rm /tmp/go1.17.5.linux-amd64.tar.gz")
-      # add go binary to path for this session
-      os.environ["PATH"] += os.pathsep + "/usr/local/go/bin"
-    
-    print("Go is installed. Proceeding...")
-  
-  # initialize psdk server
-  def __InitPSDKServer(self) -> None:
-
-    # init variables
-    validators = []
-    non_validators = []
-    data_index = 1
-
-    # create storage dir that will hold node info
-    if not os.path.isdir(os.path.dirname(__file__)+"/storage"):
-      os.mkdir(os.path.dirname(__file__)+"/storage")
-
-    # change dir to cloned folder
-    os.chdir(self.__args.clone_path)
-    # install dependances
-    subprocess.call(f"go install",shell=True)
-
-    #run init for all validators and append incement suffix for dir name if we don't have json data already
-    if os.path.isfile(os.path.dirname(__file__)+"/storage/init-validators.json") and UserInputBool("Existing validator node data found. Would you like to use the existing data ? [y/N]"):
-      print("Using existing validator data.")
-    else:
-      for _ in range(0 , self.__args.validators):
-        # delete existing directory if exists
-        if os.path.isdir(f"{self.__args.psdk_data}-{data_index}"):
-          shutil.rmtree(f"{self.__args.psdk_data}-{data_index}")
-        # init new directory and data
-        validators.append(json.loads(subprocess.run(f"go run main.go secrets init --json --data-dir {self.__args.psdk_data}-{data_index}",shell=True,capture_output=True).stdout.decode("utf-8").rstrip("\n")))
-        data_index += 1
-        # write this information to json file
-      with open(os.path.dirname(__file__)+"/storage/init-validators.json", "w") as json_file:
-        json.dump(validators, json_file, indent=4, sort_keys=True)
-      
-
-    #run init for all non validators and append incement suffix for dir name if we don't have json data already
-    if os.path.isfile(os.path.dirname(__file__)+"/storage/init-non_validators.json") and UserInputBool("Existing non validator node data found. Would you like to use the existing data ? [y/N]"):
-      print("Using existing non validator data.")
-    else:
-      for _ in range(0,self.__args.non_validators):
-         # delete existing directory if exists
-        if os.path.isdir(f"{self.__args.psdk_data}-{data_index}"):
-          shutil.rmtree(f"{self.__args.psdk_data}-{data_index}")
-        # init new directory and data
-        non_validators.append(json.loads(subprocess.run(f"go run main.go secrets init --json --data-dir {self.__args.psdk_data}-{data_index}",shell=True,capture_output=True).stdout.decode("utf-8").rstrip("\n")))
-        data_index += 1
-        # write this information to json file
-      with open(os.path.dirname(__file__)+"/storage/init-non_validators.json","w") as json_file:
-        json.dump(non_validators,json_file,indent=4,sort_keys=True)
-
-  # generate genesis.json
-  def __GenerateGenesisFile(self) -> None:
-    # change dir to psdk clone folder
-    os.chdir(self.__args.clone_path)
-
-    if os.path.isfile(f"{os.path.dirname(self.__args.psdk_data)}/genesis.json") and UserInputBool("Genesis file detected. Would you like to use the existing genesis.json file? [y/N]"):
-      print("Using the existing genesis.json file.")
-      return
-
-
-    # first part of init command
-    GenesisInitString = f"go run {self.__args.clone_path}/main.go genesis --consensus ibft"
-    # add all validator keys and boot nodes
-    for i,node in enumerate(json.load(open(os.path.dirname(__file__)+"/storage/init-validators.json"))):
-      GenesisInitString += f" --ibft-validator={node['address']} --bootnode=/ip4/127.0.0.1/tcp/{str(self.__args.libp2p_start_port+i)}/p2p/{node['node_id']}"
-
-    # add premine
-    for address in self.__args.premine_addresses:
-        GenesisInitString += f" --premine={address}:{str(self.__args.premine_funds)}"
-
-    # set block gas limit
-    if self.__args.block_gas_limit:
-        GenesisInitString += f" --block-gas-limit {self.__args.block_gas_limit}"
-
-    # now we can create genesis.json
-    os.system(GenesisInitString)
-    os.system(f"mv {os.curdir}/genesis.json {os.path.dirname(self.__args.psdk_data)}")
-    print(f"Genesis file generated at {os.path.dirname(self.__args.psdk_data)}")
-
-  # start psdk server   
-  def __StartServer(self) -> None:
-
-    # init vars
-    data_index = 0
-    validator_pids = []
-    non_validator_pids = []
-
-    # get user settings from json file
-    with open(os.path.dirname(__file__)+"/storage/config.json") as json_settings:
-      settings = json.load(json_settings)
-    
-
-    # add go binary to path for this session
-    os.environ["PATH"] += os.pathsep + "/usr/local/go/bin"
-
-    # change dir to clone dir
-    os.chdir(settings['clone_path'])
-
-    # run server command for every validator and redirect the output to file
-    for _ in json.loads(open(os.path.dirname(__file__)+"/storage/init-validators.json").read()):
-      cmd = f"go run main.go server --max-slots={settings['max_slots']} --data-dir {settings['psdk_data']}-{data_index+1} --chain {os.path.dirname(settings['psdk_data'])}/genesis.json --grpc 127.0.0.1:{settings['grpc_start_port']+data_index} --libp2p 127.0.0.1:{settings['libp2p_start_port']+data_index} --jsonrpc 127.0.0.1:{settings['json_rpc_start_port']+data_index} --seal --log-level debug"
-      validator_pids.append(subprocess.Popen(cmd, preexec_fn=os.setsid, shell=True, stdout=open(f"{os.path.dirname(settings['psdk_data'])}/node-{data_index+1}.log","w"), stderr=open(f"{os.path.dirname(settings['psdk_data'])}/node-{data_index+1}.log","w")).pid)
-      data_index += 1
-    # run server command for every non validator and redirect the output to file
-    for _ in json.loads(open(os.path.dirname(__file__)+"/storage/init-non_validators.json").read()):
-      cmd = f"go run main.go server --max-slots={settings['max_slots']} --data-dir {settings['psdk_data']}-{data_index+1} --chain {os.path.dirname(settings['psdk_data'])}/genesis.json --grpc 127.0.0.1:{settings['grpc_start_port']+data_index} --libp2p 127.0.0.1:{settings['libp2p_start_port']+data_index} --jsonrpc 127.0.0.1:{settings['json_rpc_start_port']+data_index} --log-level debug"
-      non_validator_pids.append(subprocess.Popen(cmd, preexec_fn=os.setsid, shell=True, stdout=open(f"{os.path.dirname(settings['psdk_data'])}/node-{data_index+1}.log","w"), stderr=open(f"{os.path.dirname(settings['psdk_data'])}/node-{data_index+1}.log","w")).pid)
-      data_index += 1
-    
-    # save PIDs to file
-    with open(os.path.dirname(__file__)+"/storage/validator-pids.json","w") as json_file:
-      json.dump(validator_pids,json_file,indent=4)
-        
-    with open(os.path.dirname(__file__)+"/storage/non_validator-pids.json","w") as json_file:
-      json.dump(non_validator_pids,json_file,indent=4)
-
-    print(f"All servers started! Check the logs in {os.path.dirname(settings['psdk_data'])} for activity.")
-
-  # stop psdk server
-  def __StopAllServers(self) -> None:
-
-    if not os.path.isfile(os.path.dirname(__file__)+"/storage/validator-pids.json") or not os.path.isfile(os.path.dirname(__file__)+"/storage/non_validator-pids.json"):
-      print("No servers running. You can't kill any server processes!")
-      return
-    
-    # kill portgroup of PIDs returned from running the server
-    for pid in json.loads(open(os.path.dirname(__file__)+"/storage/validator-pids.json").read()):
-      os.killpg(os.getpgid(pid),signal.SIGTERM)
-    
-    # remove PIDs file
-    os.remove(os.path.dirname(__file__)+"/storage/validator-pids.json")
- 
-    # kill portgroup of PIDs returned from running the server
-    for pid in json.loads(open(os.path.dirname(__file__)+"/storage/non_validator-pids.json").read()):
-      os.killpg(os.getpgid(pid),signal.SIGTERM)
-      
-    # remove PIDs file
-    os.remove(os.path.dirname(__file__)+"/storage/non_validator-pids.json")
-
-    
-    print("All servers stopped!")
-  
   # store user settings to file
   def __StoreSettings(self) -> None:
     settings = {}
@@ -263,6 +105,8 @@ class PsdkCommands:
     settings["premine_funds"] = self.__args.premine_funds
     settings["block_gas_limit"] = self.__args.block_gas_limit
     settings["max_slots"] = self.__args.max_slots
+    settings["hosts"] = self.__args.hosts
+    settings["environment"] = self.__args.environment
 
     # create storage dir that will hold node info
     if not os.path.isdir(os.path.dirname(__file__)+"/storage"):
